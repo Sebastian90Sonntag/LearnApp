37bcdac7de111462cf77376da003c44f17ade678
GraphicDesignCoding <111387515+GraphicDesignCoding@users.noreply.github.com>
2022-08-30 00:10:59 +0200

    Commited by Sebastian Sonntag from GraphicDesignCoding

--- app/src/main/java/com/graphicdesigncoding/learnapp/forms/InputChecker.java
+++ app/src/main/java/com/graphicdesigncoding/learnapp/forms/InputChecker.java
@@ -4,11 +4,12 @@ import android.graphics.Color;
 import android.text.Editable;
 import android.widget.EditText;
 
+import com.graphicdesigncoding.learnapp.R;
 import com.graphicdesigncoding.learnapp.api.RegExPattern;
 
 public class InputChecker{
     public boolean editText(EditText editText, String str, RegExPattern regExPattern){
-        editText.getBackground().setTint(Color.TRANSPARENT);
+        editText.getBackground().setTint(R.style.Theme_LearnApp_EditText);
         if(str.length() > 0) {
             if (regExPattern.getAction(str)) {
                 editText.getBackground().setTint(Color.GREEN);
