f78ee511ec79570ec2f3cb2c798ac2f047e9c985
AelcProgramming <111387260+AelcProgramming@users.noreply.github.com>
2022-08-29 23:47:16 +0200

    Commited by Alexander Meyer @ GraphicDesignCoding (IMG RESIZE Rework & Recover Icons)

--- app/src/main/java/com/graphicdesigncoding/learnapp/image/IMG_Resize.java
+++ app/src/main/java/com/graphicdesigncoding/learnapp/image/IMG_Resize.java
@@ -56,31 +56,14 @@ public class IMG_Resize {
         else{qp = _qual;}
     }
     public enum PIXEL{
-        X16(16),
-        X32(32),
-        X64(64),
-        X128(128),
-        X256(256),
-        X512(512),
-        X1024(1024),
-        X2048(2048),
-        X4096(4096);
+        X16(16),X32(32),X64(64),X128(128),X256(256),X512(512),X1024(1024),X2048(2048),X4096(4096);
         private final int pixel_value;
         PIXEL(int pixel_value) {
             this.pixel_value = pixel_value;
         }
     }
     public enum QUALITY_PERCENT{
-        P10(10),
-        P20(20),
-        P30(30),
-        P40(40),
-        P50(50),
-        P60(60),
-        P70(70),
-        P80(80),
-        P90(90),
-        P100(100);
+        P10(10),P20(20), P30(30), P40(40), P50(50), P60(60), P70(70), P80(80), P90(90), P100(100);
         private final int quality_value;
         QUALITY_PERCENT(int value) {
             this.quality_value = value;
